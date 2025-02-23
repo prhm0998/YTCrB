a5f2d0680a5b8188743f3f5c9c970ced21f927db
prhm0998 <myyah0998@hotmail.co.jp>
2025-02-23 18:09:45 +0900

    chore/organize-dependencies-for-workflow-prod

--- scripts/generateIcon.js
+++ scripts/generateIcon.js
@@ -3,11 +3,15 @@ import fs from 'fs-extra';
 import path from 'path';
 
 const sizes = [16, 32, 64, 96, 128];
-const inputSvgPath = './src/public/wxt.svg';
+const inputSvgPath = './ext_image/extension.svg';
 const outputDir = './src/public/icon/';
+const wxtSvgPath = './src/public/wxt.svg'; // 追加: wxt.svg のパス
 
 async function generateIcons() {
   await fs.ensureDir(outputDir); // 出力ディレクトリを作成
+
+  // inputSvgPath を wxt.svg としてコピー
+  await fs.copy(inputSvgPath, wxtSvgPath, { overwrite: true }); // overwrite: true で上書き
 
   for (const size of sizes) {
     const outputPath = path.join(outputDir, `${size}.png`);
